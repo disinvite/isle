e1f23485dced4f8ac758a1319a582fb5e9989125
Christian Semmler <mail@csemmler.com>
2023-11-20 09:40:20 -0500

    Fix checkorder issues

--- LEGO1/mxcore.h
+++ LEGO1/mxcore.h
@@ -17,17 +17,17 @@ public:
 	__declspec(dllexport) virtual MxLong Notify(MxParam& p); // vtable+04
 	virtual MxResult Tickle();                               // vtable+08
 
+	// OFFSET: LEGO1 0x100140d0
+	inline virtual MxBool IsA(const char* name) const // vtable+10
+	{
+		return !strcmp(name, MxCore::ClassName());
+	}
+
 	// OFFSET: LEGO1 0x100144c0
 	inline virtual const char* ClassName() const // vtable+0c
 	{
 		// 0x100f007c
 		return "MxCore";
-	}
-
-	// OFFSET: LEGO1 0x100140d0
-	inline virtual MxBool IsA(const char* name) const // vtable+10
-	{
-		return !strcmp(name, MxCore::ClassName());
 	}
 
 	inline MxU32 GetId() { return m_id; }
--- LEGO1/realtime/vector.h
+++ LEGO1/realtime/vector.h
@@ -3,6 +3,9 @@
 
 #include <vec.h>
 
+// OFFSET: LEGO1 0x1000c0f0 TEMPLATE
+// Vector2Impl::Vector2Impl
+
 /*
  * A simple array of three floats that can be indexed into.
  */
@@ -60,7 +63,6 @@ public:
 // SIZE 0x8
 class Vector2Impl {
 public:
-	// OFFSET: LEGO1 0x1000c0f0
 	inline Vector2Impl(float* p_data) { this->SetData(p_data); }
 
 	// vtable + 0x00 (no virtual destructor)
@@ -73,8 +75,6 @@ public:
 	virtual void MullVectorImpl(float* p_value) = 0;
 	virtual void DivScalarImpl(float* p_value) = 0;
 	virtual float DotImpl(float* p_a, float* p_b) const = 0;
-
-	// OFFSET: LEGO1 0x10002060
 	virtual void SetData(float* p_data) { this->m_data = p_data; }
 
 	// vtable + 0x20
