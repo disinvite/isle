08fa94ee05e653732273c54172a39d376f033eaf
disinvite <disinvite@users.noreply.github.com>
2024-02-08 22:44:35 -0500

    ncc variable name corrections

--- LEGO1/lego/legoomni/src/audio/legounknown100d6b4c.cpp
+++ LEGO1/lego/legoomni/src/audio/legounknown100d6b4c.cpp
@@ -23,25 +23,25 @@ LegoUnknown100d6b4c::~LegoUnknown100d6b4c()
 // FUNCTION: LEGO1 0x1003d050
 MxResult LegoUnknown100d6b4c::Tickle()
 {
-	for (Map100d6b4c::iterator map_it = m_map.begin(); map_it != m_map.end(); map_it++) {
-		LegoCacheSound* sound = (*map_it).m_sound;
+	for (Map100d6b4c::iterator mapIter = m_map.begin(); mapIter != m_map.end(); mapIter++) {
+		LegoCacheSound* sound = (*mapIter).m_sound;
 		if (sound->GetUnk0x58()) {
 			sound->FUN_10006be0();
 		}
 	}
 
-	List100d6b4c::iterator list_it = m_list.begin();
-	while (list_it != m_list.end()) {
-		LegoCacheSound* sound = (*list_it).m_sound;
+	List100d6b4c::iterator listIter = m_list.begin();
+	while (listIter != m_list.end()) {
+		LegoCacheSound* sound = (*listIter).m_sound;
 		if (sound->GetUnk0x58()) {
 			sound->FUN_10006be0();
-			list_it++;
+			listIter++;
 			continue;
 		}
 		sound->FUN_10006b80();
 
-		List100d6b4c::iterator temp = list_it;
-		list_it++;
+		List100d6b4c::iterator temp = listIter;
+		listIter++;
 
 		m_list.erase(temp);
 		delete sound;
@@ -84,28 +84,28 @@ void LegoUnknown100d6b4c::FUN_1003dc40(LegoCacheSound** p_und)
 	// SoundManager()->GetUnknown0x40()->FUN_1003dc40(&sound);
 	// LegoCacheSound*& p_sound?
 
-	for (Map100d6b4c::iterator map_it = m_map.begin(); map_it != m_map.end(); map_it++) {
+	for (Map100d6b4c::iterator mapIter = m_map.begin(); mapIter != m_map.end(); mapIter++) {
 
-		if ((*map_it).m_sound == *p_und) {
+		if ((*mapIter).m_sound == *p_und) {
 			(*p_und)->FUN_10006b80();
 
 			delete *p_und;
-			m_map.erase(map_it);
+			m_map.erase(mapIter);
 			return;
 		}
 	}
 
-	for (List100d6b4c::iterator list_it = m_list.begin();; list_it++) {
-		if (list_it == m_list.end()) {
+	for (List100d6b4c::iterator listIter = m_list.begin();; listIter++) {
+		if (listIter == m_list.end()) {
 			return;
 		}
 
-		LegoCacheSound* sound = (*list_it).m_sound;
+		LegoCacheSound* sound = (*listIter).m_sound;
 		if (sound == *p_und) {
 			(*p_und)->FUN_10006b80();
 
 			delete sound;
-			m_list.erase(list_it);
+			m_list.erase(listIter);
 			return;
 		}
 	}
