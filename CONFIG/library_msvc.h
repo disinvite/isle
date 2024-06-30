c73a255ba52d0fb02cc96d328a448b2f3157330f
disinvite <disinvite@users.noreply.github.com>
2024-06-29 21:54:55 -0400

    Library annotations

---
+++ CONFIG/library_msvc.h
@@ -0,0 +1,67 @@
+#ifdef 0
+// For CONFIG symbols only
+
+// LIBRARY: CONFIG 0x404f50
+// __onexit
+
+// LIBRARY: CONFIG 0x404ff0
+// _atexit
+
+// LIBRARY: CONFIG 0x4051d0
+// __setdefaultprecision
+
+// LIBRARY: CONFIG 0x4051f0
+// __matherr
+
+// LIBRARY: CONFIG 0x405200
+// __setargv
+
+// LIBRARY: CONFIG 0x405210
+// WinMain
+
+// LIBRARY: CONFIG 0x405229
+// AfxInitialize
+
+// LIBRARY: CONFIG 0x405256
+// _AFX_TERM_APP_STATE::_AFX_TERM_APP_STATE
+
+// GLOBAL: CONFIG 0x407000
+// ___xc_a
+
+// GLOBAL: CONFIG 0x40700c
+// ___xc_z
+
+// GLOBAL: CONFIG 0x407010
+// ___xi_a
+
+// GLOBAL: CONFIG 0x407014
+// ___xi_z
+
+// GLOBAL: CONFIG 0x408e30
+// ___win32sflag
+
+// GLOBAL: CONFIG 0x408e38
+// __dowildcard
+
+// GLOBAL: CONFIG 0x408e3c
+// __newmode
+
+// GLOBAL: CONFIG 0x408e40
+// ___defaultmatherr
+
+// GLOBAL: CONFIG 0x408e44
+// __commode
+
+// GLOBAL: CONFIG 0x408e48
+// __fmode
+
+// GLOBAL: CONFIG 0x408f64
+// __adjust_fdiv
+
+// GLOBAL: CONFIG 0x408f68
+// ___onexitend
+
+// GLOBAL: CONFIG 0x408f6c
+// ___onexitbegin
+
+#endif
