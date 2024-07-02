58e7b4835f6841d7421522c896c5fe086f1d3a3e
disinvite <disinvite@users.noreply.github.com>
2024-07-02 10:49:32 -0400

    Modern compiler fix

--- LEGO1/omni/include/mxdebug.h
+++ LEGO1/omni/include/mxdebug.h
@@ -1,8 +1,11 @@
 #ifndef MXDEBUG_H
 #define MXDEBUG_H
 
+#include "compat.h"
+
 #ifdef _DEBUG
 
+// In debug mode, replace the macro with the function call.
 #define MxTrace _MxTrace
 
 void _MxTrace(const char* format, ...);
@@ -10,7 +13,19 @@ int DebugHeapState();
 
 #else
 
+// If not debug, MxTrace is a no-op.
+
+#ifdef COMPAT_MODE
+
+// Use variadic args for macro (C99)
+#define MxTrace(...)
+
+#else
+
+// MSVC 4.20 does not have variadic args for macros
 #define MxTrace(args)
+
+#endif // COMPAT_MODE
 
 #endif // _DEBUG
 
