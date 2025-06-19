ae06820c755bf9a884dc073cc23a34415b637b39
disinvite <disinvite@users.noreply.github.com>
2025-06-19 13:47:24 -0400

    Revert "Try local variable"
    This reverts commit 5dd2fd0808f61a9d40df41242e104bc110e0b084.

--- LEGO1/tgl/d3drm/meshbuilder.cpp
+++ LEGO1/tgl/d3drm/meshbuilder.cpp
@@ -108,7 +108,7 @@ inline Result CreateMesh(
 
 	if (Succeeded(result)) {
 		rpMesh->groupIndex = groupIndex;
-		result = ResultVal(pD3DRM->SetVertices(groupIndex, 0, count, vertices));
+		result = ResultVal(pD3DRM->SetVertices(groupIndex, 0, p_numVertices, vertices));
 	}
 
 	if (!Succeeded(result)) {
